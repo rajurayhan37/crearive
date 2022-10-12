6c6256650c63b4f7ab88d1587cc1abbc4201c0a8
rajurayhan37 <rayhakobir793@gmail.com>
2022-10-12 23:26:45 +0600

    First

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+import Layout from '../components/layout'
+import { ToastContainer } from "react-toastify"
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <ToastContainer limit={1} />
+      <Component {...pageProps} />
+    </Layout>
+  )
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,51 @@
+import Head from 'next/head'
+import Message from '../components/message'
+import { useEffect, useState } from 'react'
+import { db } from "../utils/firebase"
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import Link from 'next/link';
+
+
+export default function Home() {
+  //Create a state with all the posts
+  const [ allPosts, setAllPosts ] = useState([]);
+
+  const getPosts = async () => {
+    const collectionRef = collection( db, "posts");
+    const q = query( collectionRef, orderBy("timestamp", "desc"));
+    const snap = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc)=>({ ...doc.data(), id:doc.id })));
+    })
+
+    return snap;
+  }
+
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+
+  return (
+    <div>
+      <Head>
+        <title>Creative minds | Home</title>
+        <meta name="description" content="creative mind application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className='my-8 text-lg font-medium'>
+        <h2 className='text-xl'>See what others pepole are saying</h2>
+        {allPosts.map((post) => (
+          <Message {...post} key={post.id} > 
+            <Link href={{pathname: `/${post.id}`, query: {...post}}}>
+              <button className='text-blue-600'>
+                {post.comments?.length > 0 ? post.comments?.length : 0} comments
+              </button>
+            </Link>
+          </Message>
+        ))}
+      </main>
+    </div>
+  )
+}
